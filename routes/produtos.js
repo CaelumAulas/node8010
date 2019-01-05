0725094b9b25981298d2dd864f4251060eed3968
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-01-05 13:34:32 -0200

    2.4 Mysql

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,5 +1,20 @@
 module.exports = function (app){
     app.get('/produtos', function(request,response){
-        response.render('produtos/lista')
+        //response.render('produtos/lista')
+
+        const mysql = require('mysql')
+
+        let conexao = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'casadocodigo'
+        })
+
+        conexao.query('SELECT * FROM livros', function(erro, resultados, campos){
+            response.send(resultados)
+        })
+
+        conexao.end();
     })
 }
