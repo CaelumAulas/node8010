46e07437a70b83e0a339df5daaae4a3597440f12
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-01-09 11:06:14 -0200

    Ajuste nas views

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
 module.exports = function (app) {
     app.get("/", function (request, response) {
-        response.send('<h1>Bem vindo a Casa do Código</h1>')
+        response.send(`
+            <!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <link rel="stylesheet" href="static/css/bootstrap.css">
+
+                <title>Casa do Código</title>
+            </head>
+            <body>
+            <div class="container">
+                <div class="col-12">
+                    <h1>Casa do Código</h1>
+                    <a href="/produtos" class="btn btn-primary">Produtos</a>
+                </div>
+            </div>
+            </body>
+            </html>
+        `)
     })
 }
